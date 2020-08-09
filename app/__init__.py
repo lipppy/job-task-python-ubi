071a686aa2fd06911d5e6fb39f629c58c3508270
Lipták Gábor <liptak.gabor@soulware.hu>
2020-08-09 08:18:08 +0200

    Init geopanda and check in operational use

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, request, jsonify, render_template
+
+import geopandas as gpd
+
+import numpy as np
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "<h1>Welcome to CodingX</h1>"
+
+@app.route('/timezones', methods=['GET'])
+def api_timezones():
+    # No location data available: Display all available timezones's list
+    if 'lat' not in request.args and 'lon' not in request.args:
+        tz_world_mp = gpd.read_file('app/data/timezones/tz_world_mp/tz_world_mp.shp')
+        d = np.array(tz_world_mp['TZID'].tolist())
+        e = np.unique(d)
+        # e = tz_world_mp['TZID'].unique()
+        f = {
+            'timezones': e.tolist()
+        }
+        return jsonify(f)
 
 @app.errorhandler(404)
 def page_not_found(e):
