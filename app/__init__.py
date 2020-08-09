7de1252f3e727c5f0c578a4f5c035bf1c03f9094
Lipták Gábor <liptak.gabor@soulware.hu>
2020-08-09 07:46:38 +0200

    Init 404 route

--- app/__init__.py
+++ app/__init__.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "<h1>Welcome to CodingX</h1>"
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404</h1><p>The resource could not be found.</p>", 404
