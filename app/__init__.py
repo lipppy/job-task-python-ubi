94290cd4cbb0f1ea62e675b21843a0c5d46431ad
Lipták Gábor <liptak.gabor@soulware.hu>
2020-08-10 08:28:59 +0200

    TimezoneHelper class

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,7 @@
 from flask import Flask, request, jsonify, render_template
-from shapely.geometry import Point, Polygon
+from shapely.geometry import Point
 from app.utils import is_number
-
-import geopandas as gpd
-import numpy as np
+from app.timezoneapi import TimezoneHelper
 
 app = Flask(__name__)
 
@@ -16,111 +14,60 @@ def home():
 
 @app.route('/timezones', methods=['GET'])
 def api_timezones():
+    tzh = TimezoneHelper()
     # No location data available: Display all available timezones's list
     if 'lat' not in request.args and 'lon' not in request.args:
-        # Load Timezone shapefile
-        tz_world_mp = gpd.read_file('app/data/timezones/tz_world_mp/tz_world_mp.shp')
-        d = np.array(tz_world_mp['TZID'].tolist())
-        e = np.unique(d)
-        # e = tz_world_mp['TZID'].unique()
-        f = {
-            'timezones': e.tolist()
-        }
-        return jsonify(f)
+        return jsonify(tzh.get_timezones_list())
     # Missing longitude
     elif 'lat' in request.args and 'lon' not in request.args:
-        return {
-            "code": "Missing longitude."
-        }
+        return jsonify(tzh.get_error_response(901))
     # Missing latitude
     elif 'lat' not in request.args and 'lon' in request.args:
-        return {
-            "code": "Missing latitude."
-        }
+        return jsonify(tzh.get_error_response(902))
     # Both location data available
     elif 'lat' in request.args and 'lon' in request.args:
         # Check whether lat is number
         if is_number(request.args['lat']):
             lat = float(request.args['lat'])
         else:
-            return {
-                "code": "Latitude is not a number."
-            }
+            return jsonify(tzh.get_error_response(903))
+
         # Check whether lon is number
         if is_number(request.args['lon']):
             lon = float(request.args['lon'])
         else:
-            return {
-                "code": "Longitude is not a number."
-            }
+            return jsonify(tzh.get_error_response(904))
+
         # Check whether lat is between -90.0 and +90.0
         if abs(lat) < 0.0 or abs(lat) > 90.0:
-            return {
-                "code": "Latitude is out of range."
-            }
+            return jsonify(tzh.get_error_response(905))
+
         # Check whether lon is between -180.0 and +180.0
         if abs(lon) < 0.0 or abs(lon) > 180.0:
-            return {
-                "code": "Longitude is out of range."
-            }
+            return jsonify(tzh.get_error_response(906))
 
-        # Location data passed check process
+        # Location data passed, next phase getting timezone
         location = Point(lon, lat)
 
-        # Load Timezone shapefile
-        tz_world_mp = gpd.read_file('app/data/timezones/tz_world_mp/tz_world_mp.shp')
-
-        for index, row in tz_world_mp.iterrows():
-            # if row['TZID'] == 'uninhabited':
-                # return row['geometry'].__geo_interface__
-            if location.within(row['geometry']) and row['TZID'] != "uninhabited":
-                return {
-                    "timezone": row['TZID']
-                }
+        # Check timezone
+        response = tzh.get_timezone(location)
+        if response is not None:
+            return jsonify(response)
 
         # If we reach this point then our point was not found in tz_world_mp - Check UTC
         # 1. Arg: utccheck = raw - Basic UTC - Polygons set manually
         # 2. No utccheck arg or utcheck arg not equal raw
         if 'utccheck' in request.args and request.args['utccheck'] == 'raw':
-            left = -180.0
-            right = -172.5
-            utc_polygons = []
-            utc_offsets = []
-            for i in range(-12, 12):
-                p = Polygon(
-                    [
-                        (left, 90.0),
-                        (right, 90.0),
-                        (right, -90.0),
-                        (left, -90.0)
-                    ]
-                )
-                utc_polygons.append(p)
-                utc_offsets.append('UTC' + '{:+d}'.format(i))
-
-                left = right
-                right += 15.0 if i < 12 else 7.5
-
-            utc_tz = gpd.GeoDataFrame()
-            utc_tz['offset'] = utc_offsets
-            utc_tz['geometry'] = utc_polygons
-            utc_tz = utc_tz.set_geometry('geometry')
-
-            for index, row in utc_tz.iterrows():
-                if location.within(row['geometry']):
-                    return {
-                        "timezone": row['offset']
-                    }
+            response = tzh.get_timezone_utc_raw(location)
         else:
-            # Load UTC Timezone shapefile
-            all_tz = gpd.read_file('app/data/timezones/all_tz/all_tz.shp')
+            response = tzh.get_timezone_utc(location)
 
-            for index, row in all_tz.iterrows():
-                if row['geometry'] is not None:
-                    if location.within(row['geometry']):
-                        return {
-                            "timezone": "UTC" + row['name']
-                        }
+        if response is not None:
+            return jsonify(response)
+        else:
+            return jsonify(tzh.get_error_response(907))
+
+
 
 @app.errorhandler(404)
 def page_not_found(e):
---
+++ app/timezoneapi.py
@@ -0,0 +1,120 @@
+import geopandas as gpd
+import numpy as np
+from shapely.geometry import Point, Polygon
+
+class TimezoneHelper:
+    error_responses = [
+        {"code": 900, "message": "Error while loading geometry."},
+        {"code": 901, "message": "Missing longitude."},
+        {"code": 902, "message": "Missing latitude."},
+        {"code": 903, "message": "Latitude is not a number."},
+        {"code": 904, "message": "Longitude is not a number."},
+        {"code": 905, "message": "Latitude is out of the accaptable range (between -90.0 and +90.0)."},
+        {"code": 906, "message": "Longitude is out of the accaptable range (between -180.0 and +180.0)."},
+        {"code": 907, "message": "Timezone was not found."},
+    ]
+    gdf = None
+
+    def __init__(self):
+        pass
+
+    # Load shapefile
+    def set_gdf_from_shp(self, path):
+        self.gdf = gpd.read_file(path)
+
+    def set_gdf_utc_raw(self):
+        boundary = 10 ** -5 # need because neigbours boundary are the same
+        left = -180.0
+        right = -172.5
+        utc_polygons = []
+        utc_offsets = []
+        for i in range(-12, 12):
+            p = Polygon(
+                [
+                    (left, 90.0),
+                    (right - boundary, 90.0),
+                    (right - boundary, -90.0),
+                    (left, -90.0)
+                ]
+            )
+            utc_polygons.append(p)
+            utc_offsets.append('UTC' + '{:+d}'.format(i))
+
+            left = right
+            right += 15.0 if i < 12 else 7.5
+
+        self.gdf = gpd.GeoDataFrame()
+        self.gdf['offset'] = utc_offsets
+        self.gdf['geometry'] = utc_polygons
+        self.gdf = self.gdf.set_geometry('geometry')
+
+    # All available timezones's list
+    def get_timezones_list(self):
+        try:
+            self.set_gdf_from_shp('app/data/timezones/tz_world_mp/tz_world_mp.shp')
+        except:
+            return self.get_error_response(900)
+
+        tz_list = np.array(self.gdf['TZID'].tolist())
+        tz_list_unique = np.unique(tz_list)
+        response = {
+            'timezones': tz_list_unique.tolist()
+        }
+        return response
+
+    # Timezone information
+    def get_timezone(self, location):
+        result = None
+
+        try:
+            self.set_gdf_from_shp('app/data/timezones/tz_world_mp/tz_world_mp.shp')
+        except:
+            return self.get_error_response(900)
+
+        for index, row in self.gdf.iterrows():
+            # if row['TZID'] == 'uninhabited':
+                # return row['geometry'].__geo_interface__
+            if (location.within(row['geometry']) or location.touches(row['geometry'])) and row['TZID'] != "uninhabited":
+                return {
+                    "timezone": row['TZID']
+                }
+
+        return result
+
+    # Timezone information UTC
+    def get_timezone_utc(self, location):
+        try:
+            self.set_gdf_from_shp('app/data/timezones/all_tz/all_tz.shp')
+        except:
+            return self.get_error_response(900)
+
+        for index, row in self.gdf.iterrows():
+            if row['geometry'] is not None:
+                if location.within(row['geometry']) or location.touches(row['geometry']):
+                    return {
+                        "timezone": "UTC" + row['name']
+                    }
+
+    # Timezone information UTC (raw)
+    def get_timezone_utc_raw(self, location):
+        try:
+            self.set_gdf_utc_raw()
+        except:
+            return self.get_error_response(900)
+
+        for index, row in self.gdf.iterrows():
+            if location.within(row['geometry']) or location.touches(row['geometry']):
+                return {
+                    "timezone": row['offset']
+                }
+
+    # Error response
+    def get_error_response(self, code):
+        filtered_response = [response for response in self.error_responses if response["code"] == code]
+        if len(filtered_response) == 1:
+            return filtered_response[0]
+        else:
+            return {
+                "code": code,
+                "message": "Something went wrong.",
+            }
